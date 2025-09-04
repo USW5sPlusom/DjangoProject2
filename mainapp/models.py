a693be8fc4134e03e52ad23434aaefa6347f46a8
Alexey <vaznoe2024@mail.ru>
2025-09-04 23:12:35 +0300

    v.2

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    price = models.FloatField()
+    published = models.DateField(auto_now_add=True, db_index=True)
+    
+
